08fe8b75a46a2f13124998e30ce029a10b369a90
adityaCODING-a18 <adityar18j@gmail.com>
2025-08-07 01:01:44 +0530

    fix: use ESM syntax in postcss.config.mjs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}', // Adjust this based on your folders
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
