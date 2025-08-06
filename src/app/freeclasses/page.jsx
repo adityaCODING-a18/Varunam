efd6937cabbe8a2a600fb59f5161de5a53808dff
adityaCODING-a18 <adityar18j@gmail.com>
2025-08-06 23:58:41 +0530

    fix: prevent Netlify crash by wrapping router.push in useEffect

--- src/app/freeclasses/page.jsx
+++ src/app/freeclasses/page.jsx
@@ -1,16 +1,21 @@
-"use client"
-import React from 'react'
-import { useRouter } from 'next/navigation'
+'use client'
+export const dynamic = 'force-dynamic';
+
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 const FreeClasses = () => {
   const router = useRouter();
-  router.push("https://www.youtube.com/@varunamforvictory");
+
+  useEffect(() => {
+    router.push('https://www.youtube.com/@varunamforvictory');
+  }, [router]);
 
   return (
     <div>
-      it's free classes of Varunam
+      Redirecting to free classes of Varunam...
     </div>
-  )
-}
+  );
+};
 
-export default FreeClasses
+export default FreeClasses;
